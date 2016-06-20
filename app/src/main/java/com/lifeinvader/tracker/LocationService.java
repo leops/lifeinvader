974b2991c0416356da0e8e7e956498245358bf21
Léo-Paul COUTURIER <leopaul3@gmail.com>
2016-06-20 17:00:07 +0200

    Add location service

---
+++ app/src/main/java/com/lifeinvader/tracker/LocationService.java
@@ -0,0 +1,62 @@
+package com.lifeinvader.tracker;
+
+import android.app.Service;
+import android.content.Intent;
+import android.location.Location;
+import android.os.IBinder;
+import android.util.Log;
+
+import com.google.android.gms.location.LocationRequest;
+
+import pl.charmas.android.reactivelocation.ReactiveLocationProvider;
+import rx.Subscriber;
+import rx.Subscription;
+import rx.functions.Action1;
+
+public class LocationService extends Service {
+    private Subscription mSubscription;
+
+    public LocationService() {
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("LocationService", "onStartCommand");
+        super.onStartCommand(intent, flags, startId);
+        return START_STICKY;
+    }
+
+    @Override
+    public void onCreate() {
+        Log.d("LocationService", "onCreate");
+
+        super.onCreate();
+
+        ReactiveLocationProvider locationProvider = new ReactiveLocationProvider(this);
+
+        LocationRequest request = LocationRequest.create()
+            .setPriority(LocationRequest.PRIORITY_LOW_POWER)
+            .setNumUpdates(5)
+            .setInterval(100);
+
+        mSubscription = locationProvider.getUpdatedLocation(request)
+            .subscribe(new Action1<Location>() {
+                @Override
+                public void call(Location location) {
+                    Log.d("LocationService", location.getLatitude() + ", " + location.getLongitude());
+                }
+            });
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d("LocationService", "onDestroy");
+        super.onDestroy();
+        mSubscription.unsubscribe();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
--- app/src/main/java/com/lifeinvader/tracker/activities/GroupListActivity.java
+++ app/src/main/java/com/lifeinvader/tracker/activities/GroupListActivity.java
@@ -1,14 +1,20 @@
 package com.lifeinvader.tracker.activities;
 
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
 import com.firebase.client.Firebase;
+import com.lifeinvader.tracker.LocationService;
 import com.lifeinvader.tracker.R;
 import com.lifeinvader.tracker.adapters.GroupsAdapter;
 import com.lifeinvader.tracker.models.Group;
+import com.tbruyelle.rxpermissions.RxPermissions;
+
+import rx.functions.Action1;
 
 public class GroupListActivity extends AppCompatActivity {
     public boolean twoPane;
@@ -16,7 +22,19 @@ public class GroupListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_group_list);
+
+        RxPermissions.getInstance(this)
+            .request(Manifest.permission.ACCESS_COARSE_LOCATION)
+            .subscribe(new Action1<Boolean>() {
+                @Override
+                public void call(Boolean granted) {
+                    if(granted) {
+                        startService(new Intent(GroupListActivity.this, LocationService.class));
+                    }
+                }
+            });
 
         Firebase.setAndroidContext(this);
         Firebase firebaseRef = new Firebase("https://project-4640347631861502445.firebaseio.com/");
